ee356538a8857f1926072d093bee26b5ee5852f5
Eshan <eshantiwari111@gmail.com>
2023-05-30 16:50:15 +0530

    Web Dev Practice Files Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function test() {
+    var a = "3";
+    var b = "8";
+    
+/***********Do not change the code above 👆*******/
+//Write your code on lines 7 - 9:
+    var x=a+b;
+    var a=x-a;
+    var b=x-b;    
+/***********Do not change the code below 👇*******/
+
+    console.log("a is " + a);
+    console.log("b is " + b);
+}
