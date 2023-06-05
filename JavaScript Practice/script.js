bfd75f901d06bdaca0cc8e5fca0baa37e6ea97ff
Eshan <eshantiwari111@gmail.com>
2023-06-05 13:44:04 +0530

    JS Practice Update 5th June 1

--- JavaScript Practice/script.js
+++ JavaScript Practice/script.js
@@ -55,3 +55,44 @@
 //   var change = getMilk(money);
 //   console.log(change);
 
+//BMI Calculator
+// Create your function below this line.
+// The first parameter should be the weight and the second should be the height.
+// function bmiCalculator(weight, height){
+//     var bmi = weight/(Math.pow(height,2));
+//     return Math.round(bmi);
+//   }
+//   // If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+    
+//   // This is the BMI Formula
+  
+//   /* var bmi = bmiCalculator(65, 1.8); 
+  
+//   bmi should equal 20 when it's rounded to the nearest whole number.
+  
+//   */
+//   var bmi = bmiCalculator(65,1.8);
+//   console.log(bmi);
+
+//Life Remaining
+// function lifeInWeeks(age) {
+    
+//     /************Don't change the code above************/    
+        
+//         //Write your code here.
+        
+//         //if max age is 90
+//         var days = (90 - age) * 365;
+//         var weeks = (90 - age) * 52;
+//         var months = (90 - age) * 12;
+//         console.log("You have " + days +" days, "+ weeks +" weeks, and "+ months +" months left.")
+    
+        
+        
+        
+        
+//     /*************Don't change the code below**********/
+//     }
+//     var age = prompt("What is your age?");
+//     lifeInWeeks(age)    
+
